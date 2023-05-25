f8fbe94e75a902ceaabb33b1bc0e31f54b14105a
Ddongh <qwer5383@naver.com>
2023-05-26 00:41:50 +0900

    선형회귀분석 파이썬 코드

--- server/pythons/linear.py
+++ server/pythons/linear.py
@@ -1,8 +1,20 @@
 def perform_linear_analysis(code, method, start, end):
     # 분석 작업 수행 로직을 여기에 구현합니다.
     # 예시로 간단하게 2를 반환하는 코드를 작성합니다.
-    result = 2
-    return result
+    import pandas_datareader as pdr
+
+    # 주식 코드와 기간을 지정합니다.
+    stock_code = 'AAPL'  # 애플(AAPL) 주식 코드 예시
+    start_date = '2021-01-01'
+    end_date = '2021-12-31'
+
+    # Yahoo Finance에서 주식 데이터를 가져옵니다.
+    stock_data = pdr.get_data_yahoo(stock_code, start=start_date, end=end_date)
+
+    # 가져온 주식 데이터를 출력합니다.
+    print(stock_data)
+    return stock_data
+
 
 if __name__ == "__main__":
     import sys
--- server/routes/stock.js
+++ server/routes/stock.js
@@ -12,14 +12,17 @@ router.get('/stockAnalyze', (req, res) => {
 
 router.post('/stockAnalyze/linear', (req, res) => {
   // POST 요청의 데이터는 req.body에서 가져올 수 있습니다.
-  console.log(req.body);
+  // console.log(req.body);
+  console.log("선형회귀분석 라우터 시작")
+  console.log("#### req.body #### ")
+  console.log(req.body)
   const code = req.body.code;
   const method = req.body.method;
   const start = req.body.start;
-  const end = req.body.start
-
-  const pythonProcess = spawn('python', ['server/pythons/linear.py', code, method, start, end]);
-
+  const end = req.body.end
+  
+  const pythonProcess = spawn('python3', ['server/pythons/linear.py', code, method, start, end]);
+  
   pythonProcess.stdout.on('data', (data) => {
     const output = data.toString();
     console.log(output);
@@ -28,7 +31,7 @@ router.post('/stockAnalyze/linear', (req, res) => {
     res.json({ result: output });
   });
 
-  res.json({message : "선형회귀분석 posr"});
+  // res.json({message : "선형회귀분석 posr"});
 });
 
 
