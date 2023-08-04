2297196db2b1e604d044f0885e5cfbd899b4b961
“Donghyeok <qwer5383@naver.com>
2023-08-04 10:24:33 +0900

    new branch

--- server/models/StockCodeName.js
+++ server/models/StockCodeName.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const stockCodeNameSchema = mongoose.Schema({
+const stockCodeNameSchema = mongoose.Schema({   
     code: {
         type:String
     },
